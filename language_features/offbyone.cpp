b890670f168b8c66b715ee05841c4c542956cb41
Aaron Altman <aaltmanpdx@gmail.com>
2019-02-07 22:04:39 -0800

    implementing off by one tests

--- language_features/offbyone.cpp
+++ language_features/offbyone.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+using namespace std;
+
 void offbyone()
 {
+	int i;
 	
+	cout << "For loop using postfix operator from 0 to 5: " << endl;
+	for (i = 0; i < 5; i++) {
+		cout << i << endl;
+	}
+	
+	cout << "For loop using prefix operator from 0 to 5: " << endl;
+	for (i = 0; i < 5; ++i) {
+		cout << i << endl;
+	}
+	
+	int arr[5] = { 0, 1, 2, 3, 4, 5 };
+
+	i = 0;
+	cout << "arr[i++] with i = 0: " << arr[i++] << endl;
+	cout << "resulting i: " << i << endl;
+	
+	i = 0;
+	cout << "arr[++i] with i = 0: " << arr[++i] << endl;
+	cout << "resulting i: " << i << endl;
+	
+	cout << "Testing exceptional access of i++ with i = 5 on array of length 5: " << endl;
+	i = 5;
+	cout << arr[i++];
+	
+	
+	cout << "Testing exceptional access of ++i with i = 5 on array of length 5: " << endl;
+	i = 5;
+	cout << arr[++i];
 }
