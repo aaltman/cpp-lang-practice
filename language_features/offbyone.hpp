6a4679df7f8eca931e4c9da1c5c7f93bc6c07ed5
Aaron Altman <aaltmanpdx@gmail.com>
2019-02-07 21:56:34 -0800

    Stubs

---
+++ language_features/offbyone.hpp
@@ -0,0 +1,1 @@
+void offbyone(void);
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "offbyone.hpp"
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Starting example programs..." << endl;
+	offbyone();
+}
